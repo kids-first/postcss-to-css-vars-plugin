b7af5eefd0283f0fa6dbf073cd16cad41b01a88a
Ben Dolly <bromeodolly@gmail.com>
2018-08-28 15:04:33 -0400

    :art: better dedupe color vars output

--- index.js
+++ index.js
@@ -17,20 +17,32 @@ const getVarsMapFromOpts = opts => {
   } else {
     varsMap = opts.theme;
   }
+  varsMap = _.omit(varsMap, opts.exclude);
   // overide theme vars with sub theme
   Object.assign(varsMap, opts.override ? opts.override : {});
-  // flaten our nest theme
-  varsMap = flatten(_.omit(varsMap, opts.exclude), {
+
+  // flaten our nested theme
+  varsMap = flatten(varsMap, {
     delimiter: opts.flattenDelimiter ? opts.flattenDelimiter : "-",
     safe: true
   });
   // join array with a ",": this is mostly for font stacks
   varsMap = _.mapValues(varsMap, val => (_.isArray(val) ? val.join(",") : val));
+
   // add css vars syntax to keys
   varsMap = _.mapKeys(
     varsMap,
     (v, k) => `--${opts.prefix ? opts.prefix + "-" : ""}${k}`
   );
+  let colorsMap = _.fromPairs(
+    _.toPairs(varsMap).filter(varArr => varArr[0].includes("-colors-"))
+  );
+  varsMap = _.mapValues(varsMap, (val, key) => {
+    let existingVal = _.invert(colorsMap)[val];
+    return existingVal && !key.includes("-colors-")
+      ? `var(${existingVal}, ${val})`
+      : val;
+  });
   return varsMap;
 };
 
@@ -38,8 +50,10 @@ module.exports = postcss.plugin("postcss-to-css-vars", function(opts) {
   opts = opts || {};
   let varsMap = getVarsMapFromOpts(opts);
   let logged = false;
+  let idx = 0;
   return function(root, result) {
     root.walkRules(function(rule) {
+      let selector = rule.selector;
       rule.walkDecls(function(decl) {
         // We work with each `decl` object here.
         // normalize arrays
@@ -51,15 +65,23 @@ module.exports = postcss.plugin("postcss-to-css-vars", function(opts) {
           : decl.value;
 
         const cssVar = _.invert(varsMap)[declVal];
-        decl.value = cssVar ? `var(${cssVar})` : decl.value;
+
+        decl.value = cssVar ? `var(${cssVar}, ${declVal})` : decl.value;
+        idx++;
         return decl;
       });
     });
-
+    // ADD vars to head of css doc
     root.prepend(":root{}");
 
     let themeVars = _.toPairs(varsMap).forEach(decl => {
-      root.first.prepend({ prop: decl[0], value: decl[1] });
+      root.first.append({ prop: decl[0], value: decl[1] });
+    });
+
+    root.walkRules(":root", function(rule) {
+      rule.parent
+        .insertBefore(0, { text: "vars:start" })
+        .insertAfter(1, { text: "vars:end" });
     });
   };
 });
