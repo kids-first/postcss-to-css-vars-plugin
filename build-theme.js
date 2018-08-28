7bfc8e8c1bc98de595487d69d6d0c27f2288a367
Ben Dolly <bromeodolly@gmail.com>
2018-08-28 15:38:12 -0400

    :art: modularize theme logic

---
+++ build-theme.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+var _ = require("lodash");
+var fs = require("fs");
+var path = require("path");
+var flatten = require("flat");
+
+const resolveTheme = opts => {
+  let varsMap = {};
+
+  if (
+    !_.isObject(opts.theme) &&
+    fs.existsSync(path.resolve(process.cwd(), opts.theme))
+  ) {
+    theme = require(path.resolve(process.cwd(), opts.theme));
+    varsMap = theme;
+  } else {
+    varsMap = opts.theme;
+  }
+  return varsMap;
+};
+
+const normalizeTheme = (hash, theme) => {
+  let varsHash = hash;
+  // join array with a ",": this is mostly for font stacks
+  varsHash = _.mapValues(
+    varsHash,
+    val => (_.isArray(val) ? val.join(",") : val)
+  );
+
+  // replace all color values with standard set of color vars
+  if (Object.keys(theme).includes("colors")) {
+    let colorsMap = _.fromPairs(
+      _.toPairs(varsHash).filter(varArr => varArr[0].includes("-colors-"))
+    );
+    varsHash = _.mapValues(varsHash, (val, key) => {
+      let existingVal = _.invert(colorsMap)[val];
+      return existingVal && !key.includes("-colors-")
+        ? `var(${existingVal}, ${val})`
+        : val;
+    });
+  }
+  return varsHash;
+};
+
+const getVarsMapFromOpts = opts => {
+  let theme = resolveTheme(opts);
+  let varsMap = (theme = _.omit(theme, opts.exclude));
+  // overide theme vars with sub theme
+  Object.assign(varsMap, opts.override ? opts.override : {});
+
+  // flaten our nested theme
+  varsMap = flatten(varsMap, {
+    delimiter: opts.flattenDelimiter ? opts.flattenDelimiter : "-",
+    safe: true // don't flatten array values
+  });
+
+  // add css vars syntax to keys
+  varsMap = _.mapKeys(
+    varsMap,
+    (v, k) => `--${opts.prefix ? opts.prefix + "-" : ""}${k}`
+  );
+  varsMap = normalizeTheme(varsMap, theme);
+  return varsMap;
+};
+module.exports = getVarsMapFromOpts;
--- index.js
+++ index.js
@@ -5,52 +5,7 @@ var fs = require("fs");
 var path = require("path");
 var _ = require("lodash");
 var flatten = require("flat");
-
-const getVarsMapFromOpts = opts => {
-  let varsMap = {};
-  let theme = {};
-  if (
-    !_.isObject(opts.theme) &&
-    fs.existsSync(path.resolve(process.cwd(), opts.theme))
-  ) {
-    theme = require(path.resolve(process.cwd(), opts.theme));
-    varsMap = theme;
-  } else {
-    varsMap = opts.theme;
-  }
-  varsMap = _.omit(varsMap, opts.exclude);
-  // overide theme vars with sub theme
-  Object.assign(varsMap, opts.override ? opts.override : {});
-
-  // flaten our nested theme
-  varsMap = flatten(varsMap, {
-    delimiter: opts.flattenDelimiter ? opts.flattenDelimiter : "-",
-    safe: true // don't flatten array values
-  });
-  // join array with a ",": this is mostly for font stacks
-  varsMap = _.mapValues(varsMap, val => (_.isArray(val) ? val.join(",") : val));
-
-  // add css vars syntax to keys
-  varsMap = _.mapKeys(
-    varsMap,
-    (v, k) => `--${opts.prefix ? opts.prefix + "-" : ""}${k}`
-  );
-
-  // replace all color values with standard set of color vars
-  if (Object.keys(theme).includes("colors")) {
-    let colorsMap = _.fromPairs(
-      _.toPairs(varsMap).filter(varArr => varArr[0].includes("-colors-"))
-    );
-    varsMap = _.mapValues(varsMap, (val, key) => {
-      let existingVal = _.invert(colorsMap)[val];
-      return existingVal && !key.includes("-colors-")
-        ? `var(${existingVal}, ${val})`
-        : val;
-    });
-  }
-
-  return varsMap;
-};
+var getVarsMapFromOpts = require("./build-theme.js");
 
 module.exports = postcss.plugin("postcss-to-css-vars", function(opts) {
   opts = opts || {};
